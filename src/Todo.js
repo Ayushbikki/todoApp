834214fcbcb6ba0a44321bb208ae368790f94db8
Ayush <ayush.bikki222@gmail.com>
2022-05-23 19:17:36 +0530

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import './Todo.css';
+
+import Task from './Task';
+import CreateTask from './CreateTask';
+
+                
+function Todo() {
+    const [tasksRemaining, setTasksRemaining] = useState(0);
+    const [tasks, setTasks] = useState([
+        {
+            title: "play cricket",
+            completed: true
+        },
+        {
+            title: "watch television",
+            completed: true
+        },
+        {
+            title: "Repair your gadget",
+            completed: false
+        }
+    ]);
+      
+    useEffect(() => { 
+      setTasksRemaining(tasks.filter(task => !task.completed).length) 
+    });
+
+    const addTask = title => {
+        const newTasks = [...tasks, { title, completed: false }];
+        setTasks(newTasks);
+    };
+    
+    const completeTask = index => {
+        const newTasks = [...tasks];
+        newTasks[index].completed = true;
+        setTasks(newTasks);
+    };
+    
+    const removeTask = index => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    };
+
+    return (
+        <div className="todo-container">
+            <div className="header">Pending tasks ({tasksRemaining})</div>
+            <div className="tasks">
+                {tasks.map((task, index) => (
+                    <Task
+                    task={task}
+                    index={index}
+                    completeTask={completeTask}
+                    removeTask={removeTask}
+                    key={index}
+                    />
+                ))}
+            </div>
+            <div className="create-task" >
+                <CreateTask addTask={addTask} />
+            </div>
+        </div>
+    );
+}
+export default Todo
